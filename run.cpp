babacca8d00e57e2a57762d37880893b6a42c261
Ojas <OjasD025@gmail.com>
2026-08-03 23:22:51 +0530

    Add gemma4-e2b-it artifacts and quantization experiments

--- run.cpp
+++ run.cpp
@@ -2,6 +2,47 @@
 
 using namespace std;
 
+// ___________________________________________________________
+// CONFIGURATIONS
+// ___________________________________________________________
+
+typedef struct {
+
+} Config;
+
+typedef struct {
+
+} Weights;
+
+typedef struct {
+
+} RuntimeState;
+
+typedef struct {
+
+} Transformer;
+
+// ___________________________________________________________
+
+void malloc_runtime_state();
+void free_runtime_state();
+void load_checkpoint();
+void map_weights();
+
+// ___________________________________________________________
+// TRANSFORMER OPS & TOOLING
+// ___________________________________________________________
+
+void RMSNorm();
+
+
+
+// ___________________________________________________________
+void forward();
+
+// ___________________________________________________________
+void generate();
+
 int main() {
     cout << "Gemma runtime" << endl;
 }
